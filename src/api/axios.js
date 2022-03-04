6cfe94f4cf7ce7aee400fa5241ff5a0d7b2d2148
Mori <dev@8bell.com>
2022-03-04 21:20:37 +0900

    NETFLIX clone v1.5.0

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        api_key: "ce1fb6e7c3a8d77dec122e525aff0db5",
+        language:"ko-KR",
+    },
+});
+
+export default instance;
---
+++ src/api/requests.js
@@ -0,0 +1,12 @@
+const requests = {
+    fetchNowPlaying: "movie/now/playing",
+    fetchNetflixOriginals: "/discover/tv?with_networks=213",
+    fetchTranding: "/trending/all/week",
+    fetchTopRated: "/movie/top_rated",
+    fetchActionMovies: "/discover/movie?with_geners=28",
+    fetchComedyMovies: "/discover/movie?with_geners=35",
+    fetchHorrorMovies: "/discover/movie?with_geners=27",
+    fetchRomanceMovies: "/discover/movie?with_geners=10749",
+    fetchDocumentaries: "/discover/movie?with_geners=99",
+}
+export default requests;
