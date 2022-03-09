1f3a13a9c14e4f82e0960eac3f5f91c58ec6b7ef
Mori <dev@8bell.com>
2022-03-09 20:51:01 +0900

    NETFLIX clone v2.2.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import requests from './api/requests';
+import Row from './components/Row';
 
 function App() {
   return (
@@ -9,9 +10,37 @@ function App() {
       
       <Nav/>
       <Banner/>
+      <Row
+      title = 'NETFILX ORIGINALS'
+      id = 'NO'
+      fetchURL = {requests.fetchNetflixOriginals}
+      isLargeRow
+      />
+      <Row
+      title = 'TRENDING NOW'
+      id = 'TN'
+      fetchURL = {requests.fetchTrending}
+      />
 
-
+      <Row
+      title = 'TOP RATED'
+      id = 'TR'
+      fetchURL = {requests.fetchTopRated}
       
+      />
+      <Row
+      title = 'ACTION MOVIES'
+      id = 'AM'
+      fetchURL = {requests.fetchActionMovies}
+    
+      />
+       <Row
+      title = 'COMEDY MOVIES'
+      id = 'CM'
+      fetchURL = {requests.fetchComedyMovies}
+    
+      />
+
     </div>
   );
 }
