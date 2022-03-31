74671724f6295b20cf83d1ee87dfa6e7e017b37e
Mori <dev@8bell.com>
2022-04-01 03:59:01 +0900

    NETFLIX clone v4.4.3

--- src/components/MovieModal/Star.js
+++ src/components/MovieModal/Star.js
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { FaStar } from 'react-icons/fa';
-import styled from 'styled-components';
+import './Star.css';
 
 const ARRAY = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+let score = 0;
 export default function Rating() {
 	const [clicked, setClicked] = useState([
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
 	]);
 
 	const handleStarClick = (index) => {
@@ -23,47 +22,48 @@ export default function Rating() {
 		for (let i = 0; i < 10; i++) {
 			clickStates[i] = i <= index ? true : false;
 		}
+		score = clickStates.filter(Boolean).length;
 		setClicked(clickStates);
 	};
 
-	useEffect(() => {
-		sendReview();
-	}, [clicked]);
-
-	const sendReview = () => {
-		let score = clicked.filter(Boolean).length;
-	};
-
 	return (
-		<div>
-			<h1 className='rate'>{clicked}</h1>
+		<div
+			className=' rate_box
+    '>
+			<p>별점을 선택해주세요.</p>
 			<div className='input__rating'>
-				<div className='backStar_wrapper'>
-					<img className='backStar' src='/images/grayStar.png' />
-					<img className='backStar' src='/images/grayStar.png' />
-					<img className='backStar' src='/images/grayStar.png' />
-					<img className='backStar' src='/images/grayStar.png' />
-					<img className='backStar' src='/images/grayStar.png' />
-				</div>
-				<div className='yellowStar_wrapper'>
-					{ARRAY.map((el, idx) => {
-						return (
-							<img
-								key={idx}
-								onClick={() => handleStarClick(el)}
-								className={
-									clicked[el]
-										? idx % 2 === 0
-											? 'yellowStar'
-											: 'yellowStar--reverse'
-										: 'grayStar'
-								}
-								src='/images/yellowStarHalf.png'
-							/>
-						);
-					})}
+				<div className='Star_wrapper'>
+					{/* <div className='backStar_wrapper'>
+						<img className='backStar' src='/images/yellowStar.png' />
+						<img className='backStar' src='/images/yellowStar.png' />
+						<img className='backStar' src='/images/yellowStar.png' />
+						<img className='backStar' src='/images/yellowStar.png' />
+						<img className='backStar' src='/images/yellowStar.png' />
+					</div> */}
+					<div className='yellowStar_wrapper'>
+						{ARRAY.map((el, idx) => {
+							return (
+								<img
+									id={idx}
+									key={idx}
+									onClick={() => handleStarClick(el)}
+									className={
+										clicked[el]
+											? idx % 2 === 0
+												? 'yellowStar'
+												: 'yellowStar--reverse'
+											: idx % 2 === 0
+											? 'grayStar'
+											: 'grayStar--reverse'
+									}
+									src='/images/yellowStarHalf.png'
+								/>
+							);
+						})}
+					</div>
 				</div>
 			</div>
+			<p className='rate'>{score / 2}</p>
 		</div>
 	);
 }
